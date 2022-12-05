dd13cc4e334988190fa0af65776cea28ea1aa0d7
dynamo58 <sailorfirex@seznam.cz>
2022-12-05 01:58:56 +0100

    xd

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
+use std::io::Read;
+
 use pttypter::lexer::lex;
 use pttypter::parse_args;
 use pttypter::printer::{html, term, Output, ThemeKind};
 
 fn main() {
+    #[cfg(target_os = "windows")]
+    ansi_term::enable_ansi_support().expect("Error: failed to enable ANSI");
+
     let args = parse_args().unwrap_or_else(|e| {
         let mut input = String::new();
-        std::io::stdin().lines().for_each(|l| {
-            input.push_str(&l.unwrap_or_else(|_| {
-                println!("Error: Failed to read input (also no arguments provided)");
-                std::process::exit(1);
-            }))
-        });
+        std::io::stdin()
+            .read_to_string(&mut input)
+            .expect("Error: failed to read input");
+
         if input != String::new() {
             term::print(lex(input), ThemeKind::Dark);
             std::process::exit(0);
