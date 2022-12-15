fada879b2c73415712894fbfd6af1427e2eb300f
dynamo58 <sailorfirex@seznam.cz>
2022-12-15 15:59:22 +0100

    x

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@
  cargo build --release --target x86_64-pc-windows-gnu
  cargo build --release --target x86_64-unknown-linux-gnu
 
-# zip
-rm bin.zip
-zip -r bin bin
-
 # move
  cp ./target/x86_64-pc-windows-gnu/release/pttypter.exe bin/
  cp ./target/x86_64-unknown-linux-gnu/release/pttypter bin/
+
+# zip
+rm bin.zip
+zip -r bin bin
 
 # clean
 rm ./docs/**/*.aux &>/dev/null
@@ -19,5 +19,4 @@ rm ./docs/**/*.fls &>/dev/null
 rm ./docs/**/*.log &>/dev/null
 rm ./docs/**/*.out &>/dev/null
 rm ./docs/**/*.gz &>/dev/null
-
 
